f8c84679ba9b0decac1d7e0fde64c7a6d2d8efa2
Nicholas Brunhart-Lupo <nicholasbl@icloud.com>
2020-02-03 13:25:19 -0700

    Improved single header generator

--- src/make_single_header.py
+++ src/make_single_header.py
@@ -1,12 +1,35 @@
-import sys
+#!/usr/bin/env python3
+
+# Build a single header out of a series of given files.
+# Files are scanned for non-system includes, and everything is packed into the destination.
+
+# TODO: Clean up include detection
+
 import os
 import networkx as nx
 import re
+import glob
+import argparse
 
-srcs = [ os.path.abspath(i) for i in sys.argv[1:-1] ]
-dest = sys.argv[-1]
+parser = argparse.ArgumentParser(description='Pack includes into a single header')
 
-print(srcs, dest)
+parser.add_argument('sources', metavar='I', nargs='+',
+                    help = 'List of .h or .hpp files to pack')
+
+parser.add_argument('destination',
+                    help = 'destination file to write packed headers to')
+
+args = parser.parse_args()
+
+# print(args.sources)
+# print(args.destination)
+
+srcs = []
+
+for i in args.sources:
+    srcs += [ os.path.abspath(j) for j in glob.glob(i) ]
+
+dest = args.destination
 
 outf = open(dest, 'w')
 
@@ -21,7 +44,7 @@ def is_rel_include(line):
 for f in srcs:
     container = os.path.dirname(f)
     # probe
-    print("Considering: ",f)
+    print("Considering:",f)
     deps = [ l for l in open(f) if l.strip().startswith("#include") ]
     deps = [ l.replace("#include","").strip() for l in deps ]
     deps = [ l.replace("\"","") for l in deps if not l.startswith("<")]
@@ -29,13 +52,16 @@ for f in srcs:
     print("Depends on: ")
     for l in deps:
         G.add_edge(l,f)
-        print(" - ", l)
+        print(" -", l)
+    print("")
 
 print("Writing single header:")
 
 for f in nx.topological_sort(G):
-    print("- Adding ", f)
+    print(" - Adding", f)
     for l in open(f):
         is_inc = is_rel_include(l)
         if not is_inc:
             outf.write(l)
+
+print("Written to: {}".format(dest))
